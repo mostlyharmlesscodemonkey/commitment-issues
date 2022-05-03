d362c87ec2c738ba3e80c720d26b0a84e01f0dde
mostlyharmlesscodemonkey <mostlyharmlesscodemonkey@gmail.com>
2022-05-03 14:33:21 -0400

    Rename dummy test files.

--- test_unittest.py
+++ test_unittest.py
@@ -2,15 +2,15 @@
 # unit tests
 
 # import code to test
-import app
+import dummy
 import unittest
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
-        self.assertEqual(app.increment(3), 4)
+        self.assertEqual(dummy.increment(3), 4)
     
     def test_decrement(self):
-        self.assertEqual(app.decrement(3), 2)
+        self.assertEqual(dummy.decrement(3), 2)
 
 if __name__ == '__main__':
     unittest.main()
