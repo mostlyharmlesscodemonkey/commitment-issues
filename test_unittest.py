450fd3b679b0d48153e2bdc51eadbf5391d56261
mostlyharmlesscodemonkey <mostlyharmlesscodemonkey@gmail.com>
2022-05-04 00:56:26 -0400

    Added more repo actions like adding files.

--- test_unittest.py
+++ test_unittest.py
@@ -1,7 +1,10 @@
 # unit tests
 
+# TODO: need to create some way to inject the client through an interface because these
+# all make network calls to the real api so aren't really unit tests.
+
 # import code to test
-import committer
+from committer import Committer
 import unittest
 import json
 
@@ -10,11 +13,32 @@ class Test_TestGithubOperations(unittest.TestCase):
         # read credential file for access token
         f = open('local.credentials.json')
         d = json.load(f)
-        self.access_token = d['access_token']
         f.close()
+        self.app = Committer(d['access_token'])
+        self.repo_name = d['repo_name']
 
     def test_count_repos(self):
-        self.assertEqual(committer.count_repos(self.access_token), 1)
+        self.assertEqual(self.app.count_repos(), 1)
+
+    def test_has_master_branch_positive(self):
+        self.assertEqual(self.app.has_master_branch(self.repo_name), True)
+
+    def test_has_master_branch_negative(self):
+        self.assertEqual(self.app.has_master_branch('non-existent-repo'), False)
+    
+    def test_generate_filename_unique(self):
+        gen_1 = self.app.generate_filename('prefix')
+        gen_2 = self.app.generate_filename('prefix')
+        self.assertNotEqual(gen_1, gen_2)
+
+    def test_commit_add_file(self):
+        file_name = self.app.generate_filename('prefix')
+        self.assertIsNotNone(self.app.commit_add_file(self.repo_name, "test", "test: added a new file", file_name=file_name, data="wowee"))
+    
+    # delete file on branch
+
+    # merge open PR automagically?
+
 
 if __name__ == '__main__':
     unittest.main()
